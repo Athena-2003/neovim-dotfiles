dc6ae199e4bba67567f14d2818e3d3a2859df232
Shashank B N <bnshashankr1597@gmail.com>
2024-01-06 17:12:16 +0530

    switched colorscheme to solarized-osaka (testing)

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,9 +1,19 @@
 return {
-	{ 'rose-pine/neovim', name = 'rose-pine' },
+	-- { 'rose-pine/neovim', name = 'rose-pine' },
+	-- {
+	-- 	"LazyVim/LazyVim",
+	-- 	opts = {
+	-- 		colorscheme = "rose-pine",
+	-- 	},
+	-- 	},
 	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "rose-pine",
-		},
-		},
+		"craftzdog/solarized-osaka.nvim",
+		lazy = true,
+		priority = 1000,
+		opts = function()
+			return {
+				transparent = true,
+			}
+		end,
+},
 }
