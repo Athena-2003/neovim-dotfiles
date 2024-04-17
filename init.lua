dc1af7ebbd94199e7d718d31991b066926c085b6
Athena-2003 <bnshashankr1597@gmail.com>
2024-04-17 20:21:41 +0530

    making git coconut oil smooth

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
---[[ini
+--[[
 
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
@@ -152,6 +152,7 @@ require('lazy').setup({
 				vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
 				vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
 				vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+				vim.keymap.set('n', '<leader>gb', require('gitsigns').toggle_current_line_blame, {buffer = bufnr, desc = 'Toggle [G]it [B]lame'})
 			end,
 		},
 	},
