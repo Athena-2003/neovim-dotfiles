132f238eacdc5bf5f66ff7d1c0e28c714383476e
Athena-2003 <bnshashankr1597@gmail.com>
2024-04-30 19:38:42 +0530

    keymap for lsp formatting

--- init.lua
+++ init.lua
@@ -492,6 +492,7 @@ local on_attach = function(_, bufnr)
 	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
 		vim.lsp.buf.format()
 		end, { desc = 'Format current buffer with LSP' })
+	vim.keymap.set('n', '<leader>f', ':Format<CR>' )
 end
 
 -- Enable the following language servers
